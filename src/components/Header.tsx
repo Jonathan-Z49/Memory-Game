8d3e8ad3dbf8bed220abbc77940f71a269eb3d33
Jonathan Zarate <jonathanz49.dev@gmail.com>
2022-12-05 02:10:31 -0600

    Updated header component to have a inner span child so that text could be aligned vertically/horizontally

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
 export const Header = () => {
-  return <header className="head">Breaking Bad Memory Game!</header>;
+  return (
+    <header className="head">
+      <span className="head-text">Breaking Bad Memory Game!</span>
+    </header>
+  );
 };
