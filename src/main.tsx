33629311c4a51be0884bf1637099548864aa4650
Jonathan Zarate <jonathanz49.dev@gmail.com>
2022-12-04 22:00:44 -0600

    Added header and footer to main component

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,15 @@
-import './css/index.css';
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './components/App';
+import Footer from './components/Footer';
+import { Header } from './components/Header';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Header />
     <App />
+    <Footer />
   </React.StrictMode>,
   document.getElementById('root'),
 );
