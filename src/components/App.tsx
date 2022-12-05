a85ff282b0503edcf7d5c721175e83c0ee36b0d8
Jonathan Zarate <jonathanz49.dev@gmail.com>
2022-12-05 02:09:12 -0600

    Removed unused code, added reset function, added WinModal, added loading message while API data is waiting to be recieved.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,58 +1,71 @@
+import 'modern-normalize';
 import '../css/App.scss';
 
 import React, { useEffect, useState } from 'react';
 
-import getData from '../utils/api';
-import Card from './Card';
+import shuffle from '../utils/shuffle';
+import CardContainer from './CardContainer';
+import Scoreboard from './Scoreboard';
+import WinModal from './WinModal';
 
 function App() {
   const [characters, setCharacters] = useState<CharacterModel[]>([]); //api data
-  const [score, setScore] = useState<number>(0); //score
+  const [score, setScore] = useState<Scores>({ current: 0, best: 0 }); //score
   const [guess, setGuess] = useState<number[]>([]); //current guesses, by char_id from api data
   const [loading, setLoading] = useState(false); //if api loaded or not
 
   useEffect(() => {
-    // if (!loading) {
-    //   const offset: number = Math.floor(Math.random() * (62 - 0) + 0);
-    //   const url = `https://www.breakingbadapi.com/api/characters?limit=10&offset=${offset}`;
-    //   fetch(url)
-    //     .then((res) => res.json())
-    //     .then((data) => {
-    //       setCharacters(data);
-    //       setLoading(true);
-    //     })
-    //     .catch((error) => {
-    //       setLoading(false);
-    //       console.log(error);
-    //     });
-    // }
-  }, []);
+    if (!loading) {
+      const offset: number = Math.floor(Math.random() * (52 - 0) + 0); //62 chars in api, make limit 52 so that we never have less than 10 chars
+      const url = `https://www.breakingbadapi.com/api/characters?limit=10&offset=${offset}`;
+      fetch(url)
+        .then((res) => res.json())
+        .then((data) => {
+          setCharacters(data);
+          setLoading(true);
+        })
+        .catch((error) => {
+          setLoading(false);
+          console.log(error);
+        });
+    } else {
+      setCharacters((prevState) => shuffle(prevState.map((obj) => ({ ...obj })))); //shuffle copy of array
+    }
+  }, [guess]);
+
+  const resetGame = () => {
+    setGuess([]); //incorrect guess, lost game so reset.
+    setScore((prevState) => ({ ...prevState, current: 0 })); //only reset regular score, not best score.
+    setLoading(false); //get new api data
+  };
 
   const handleClickGuess = (e: React.MouseEvent<HTMLElement>) => {
-    const char_id: string = (e.target as HTMLDivElement).getAttribute('data-id');
+    const char_id: string = e.currentTarget.getAttribute('data-id');
     const isPresent = guess.indexOf(Number(char_id)); //index or -1 if not found
+    console.log(char_id);
+    console.log(Number(char_id));
+
     if (isPresent == -1) {
       setGuess((prevState) => [...prevState, Number(char_id)]); //correct guess
-      setScore((prevState) => prevState + 1);
+      setScore((prevState) => ({ ...prevState, current: prevState.current + 1 })); //add to score if correct
+      if (score.current > score.best) {
+        setScore((prevState) => ({ ...prevState, best: prevState.current })); //update high score
+      }
     } else {
-      setGuess([]); //incorrect guess, lost game so reset.
-      setScore(0);
-      setLoading(false); //get new api data
+      resetGame();
     }
   };
 
   return (
-    <div className="App">
-      {characters.map((item) => (
-        <Card
-          key={item.char_id}
-          name={item.name}
-          image={item.img}
-          id={item.char_id}
-          onClick={handleClickGuess}
-        />
-      ))}
-    </div>
+    <main className="App">
+      <Scoreboard scores={score} />
+      {loading ? (
+        <CardContainer characters={characters} onClick={handleClickGuess} />
+      ) : (
+        <div className="loading-msg">Loading...</div>
+      )}
+      {score.current === 10 && <WinModal onClick={resetGame} />}
+    </main>
   );
 }
 
