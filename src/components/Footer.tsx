373f12098cae7ad08c7cd77b69f8934c2442475e
Jonathan Zarate <jonathanz49.dev@gmail.com>
2022-12-05 02:09:31 -0600

    Updated footer anchor link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer>
-      <a href="https://github.com/Jonathan-Z49/CV-App">
+    <footer className="foot">
+      <a href="https://github.com/Jonathan-Z49/Memory-Game">
         <img
           src="https://upload.wikimedia.org/wikipedia/commons/c/cd/GitHub-Mark-64px.png?20201220123257"
           alt="Github logo"
