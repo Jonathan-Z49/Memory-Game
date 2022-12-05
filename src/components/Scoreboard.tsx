527d8c8d2618195c06ffa172390a7f4a6621e614
Jonathan Zarate <jonathanz49.dev@gmail.com>
2022-12-05 02:12:09 -0600

    Added effect so that the scoreboard button animates based on a score change

--- src/components/Scoreboard.tsx
+++ src/components/Scoreboard.tsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { MutableRefObject, useEffect, useRef } from 'react';
 
 const Scoreboard = (props: { scores: Scores }) => {
+  const score_container = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (!score_container.current?.classList.contains('active')) {
+      score_container.current?.classList.add('active');
+    }
+    const timer = setTimeout(() => {
+      score_container.current?.classList.remove('active');
+    }, 250);
+    return () => clearTimeout(timer);
+  }, [props.scores.current, props.scores.best]);
+
   return (
-    <div className="scoreboard">
-      <span className="current-score"> {props.scores.current} </span>
-      <span className="best-score"> {props.scores.best} </span>
+    <div ref={score_container} className="scoreboard">
+      <span className="current-score"> {'Score: ' + props.scores.current} </span>
+      <span className="best-score"> {'Best score: ' + props.scores.best} </span>
     </div>
   );
 };
