a5c8e7212f2518aa3615a40278c13542d3a0757b
Jonathan Zarate <jonathanz49.dev@gmail.com>
2022-12-04 21:54:21 -0600

    Added header and footer component.

---
+++ src/components/Footer.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <a href="https://github.com/Jonathan-Z49/CV-App">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/c/cd/GitHub-Mark-64px.png?20201220123257"
+          alt="Github logo"
+        />
+      </a>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/Header.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const Header = () => {
+  return <header className="head">Breaking Bad Memory Game!</header>;
+};
