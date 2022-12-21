32df0cfde696958a0ee8c6b65c7ff22551cbd3bd
Jonathan Zarate <jonathanz49.dev@gmail.com>
2022-12-21 01:19:23 -0600

    Changed title

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const Header = () => {
   return (
     <header className="head">
-      <span className="head-text">Breaking Bad Memory Game!</span>
+      <span className="head-text">Pokemon Memory Game!</span>
     </header>
   );
 };
